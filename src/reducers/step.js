ea4cad4594d435e4cc7b264430263475d0053830
Piotr <galaspiotrek@gmail.com>
2019-02-14 23:12:05 +0100

    Fix immutable state

--- src/reducers/step.js
+++ src/reducers/step.js
@@ -2,9 +2,9 @@ const defaultNextStepState = { activeStep: 0 }
 export default function nextStep(state=defaultNextStepState, action){
   switch (action.type) {
     case 'NEXT_STEP':
-      return Object.assign(state, action.payload)
+      return Object.assign({}, state, action.payload)
     case 'PREVIOUS_STEP':
-      return Object.assign(state, action.payload)
+      return Object.assign({}, state, action.payload)
     default:
       return state;
   }
