33a4834a3e992dbed5777ad01685741f59bb1d63
Piotr <galaspiotrek@gmail.com>
2019-04-09 23:32:30 +0200

    Fix timezone for range-date-picker

--- src/containers/content/datePicker.jsx
+++ src/containers/content/datePicker.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import DatePickerComponent from '../../components/content/datePicker'
 import { connect } from 'react-redux'
 import { selectRange } from '../../actions/dateRange'
+import moment from 'moment'
+
 
 class DatePickerContainer extends React.Component {
+  // formattedRange fixes timezone problem
   handleSelect(range){
-    this.props.selectRange(range)
+    const startDate = moment(range.selection.startDate).add(4, "h").toDate()
+    const endDate = moment(range.selection.endDate).add(4, "h").toDate()
+    const formattedRange = { selection: {startDate, endDate, key: "selection"}}
+    this.props.selectRange(formattedRange)
   }
 
   render(){
